6a1ae92296e6eee4837f01fa93e0af7e9310593f
Arthur Silva <aretw0@hotmail.com>
2018-08-03 03:56:59 -0300

    Melhorias em product

--- src/sd1/commons/Product.java
+++ src/sd1/commons/Product.java
@@ -34,7 +34,7 @@ public class Product {
 	static final int MINC = 100;
 	static final int MAXC = 999;
 	
-	private Random generator = new Random();
+//	private Random generator = new Random();
 	
 	public String getName() {
 		return name;
@@ -73,6 +73,10 @@ public class Product {
 		--this.amount;
 	}
 	
+	public boolean equals(Product p) {
+		return ((p.getCod() == this.cod)&&(p.getName() == this.name)&&(p.getAmount() == this.amount) && (p.getType() == this.type)&&(p.getPrice() == this.price));
+	}
+	
 	public void setProduct(Product p) {
 		this.cod = p.getCod();
 		this.type = p.getType();
@@ -100,12 +104,12 @@ public class Product {
 	}
 	
 	// usado so ao transferir 
-	public Product(int cod,int type, double price, String name, int amount) {
-		this.cod = cod;
-		this.type = type;
-		this.price = price;
-		this.name = name;
-		this.amount = amount;
+	public Product(ProductTemp p) {
+		this.cod = Product.codSeed++;
+		this.type = p.getType();
+		this.price = p.getPrice();
+		this.name = p.getName();
+		this.amount = p.getAmount();
 	}
 	
 	@Override
@@ -116,16 +120,19 @@ public class Product {
 	/*public static void main(String[] args) {
 		
 		Product p1 = new Product(Product.FOOD,7.89,"Frango");
+//		Product p2 = new Product(p1);
+//		
+//		System.out.println(p1.toString());
 		
-		System.out.println(p1.toString());
+//		String json = JsonTools.gsonExpose.toJson(p1);
 		
-		String json = JsonTools.gsonExpose.toJson(p1);
+//		System.out.println(json);
 		
-		System.out.println(json);
+//		Product p2 = JsonTools.gsonExpose.fromJson(json, Product.class);
 		
-		Product p2 = JsonTools.gsonExpose.fromJson(json, Product.class);
-		
-		System.out.println(p2.toString());
+//		System.out.println(p2.toString());
+//		
+//		System.out.println(p2.equals(p1));
 		
 	}*/
 
